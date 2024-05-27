54e41efc94b93dfa01c335204f7643e5721a5080
vinbech <vinbech@yahoo.com>
2024-05-27 21:33:19 +0200

    Set up an AI class
    When we figure out how to implement all of these functions, we should have a functioning AI. This is according to how the book explains it.

--- containers/Move.java
+++ containers/Move.java
@@ -2,6 +2,12 @@ package containers;
 import game.Land;
 import players.Player;
 
+/**
+ * Move represents the action of moving troops.
+ * A Move can either be a movement between connected friendly lands of "from",
+ * or an attack to a neighbouring hostile Land of "from".
+ * Count indicates how many troops is part of the Move action.
+ */
 public class Move {
     public Player player;
     public Land from;
