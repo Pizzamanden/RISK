9b544556b01c1fb17fe954d5d2ea7c689de1bc89
Peter Winther <dialga399@live.dk>
2024-05-29 03:46:24 +0200

    more work

--- players/AI.java
+++ players/AI.java
@@ -38,6 +38,9 @@ public abstract class AI extends Player{
      *  For each of these zone, there is a pool of troops available, which can:
      *  - Attack an enemy land which neighbours that zone
      *  - Be placed on the border of land in the zone, which borders an enemy land
+     *  So the total combinations become:
+     *  - Each zone has an amount of troops
+     *  - For each bordering land in this zone, there can be made any combination of troops
      */
     public ArrayList<Move> generateMoveList(Board board){
         ArrayList<Move> moveList = new ArrayList<>();
@@ -50,22 +53,33 @@ public abstract class AI extends Player{
             int zoneTroopCount = zone.get(0).getConnectedMoveableTroopCount(zone) - (zone.get(0).getTroopCount() - 1);
             // With this troop count in a zone, we should now find the border.
             ArrayList<Land> borderLands = new ArrayList<>();
-            for (Land land : zone) {
+            for (Land land : zone) { // We do not use the method in board, as we only care about bordering lands in this specific zone
                 if(land.hasEnemyNeighbour()){
                     borderLands.add(land);
                 }
             }
             // For each of these bordering lands, we can divide up the troops in this zone in all permutations
-            
-            // This constitues leaving troops on the border
+            for (ArrayList<Land> arrayList : connectedZones) {
+                
+            }
+            // This constitues leaving troops on the border, or performing all possible attacks on all enemy lands which border this zone
             // We could also attack, and even do a mix of attacking and leaving troops on the border
             // We are really only interested making one attack on an enemy land per zone
             // We really just need to generate a move for each possible attack, for each possible enemy land target
-
+            HashSet<Land> enemyLandTargets = new HashSet<>();
+            for (Land borderland : borderLands) {
+                enemyLandTargets.addAll(borderland.getHostileNeighbours());
+            }
+            // Add all attacks from 1 troop up to Math.min(3, zoneTroopCount)
+            for (Land target : enemyLandTargets) {
+                // Find an allied neighbour to this land, to designate as the source of the attack
+                Land source = target.getHostileNeighbours().get(0); // This is allowed, as there is always one neighbour which is hostile to this hostile land, us. Otherwise we would never have found this land to begin with
+                for (int i = 1; i <= Math.min(3, zoneTroopCount); i++) {
+                    moveList.add(new Move(this, source, target, i));
+                }
+            }
 
         }
-
-
         return moveList;
     }
 }
